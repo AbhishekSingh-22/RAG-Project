54e436ed9a5a22639bfa56232ac8a5d859c41cbe
Abhishek <abhisheksingh.vizag@gmail.com>
2025-12-30 12:10:34 +0530

    Clean up

--- src/document_loading_Unstructured.py
+++ src/document_loading_Unstructured.py
@@ -5,10 +5,9 @@ ROOT_DIR = Path().resolve() # Root directory of the project
 DATA_DIR = ROOT_DIR.joinpath("data") # Data directory
 PDF_DIR = DATA_DIR.joinpath("pdfs") # PDFs directory
 
-loader = UnstructuredPDFLoader("D:\Panasonic\main_project\data\pdfs\CS_NU18VKYW.pdf", mode="paged")   # uses partition_pdf internally
+loader = UnstructuredPDFLoader("D:\Panasonic\main_project\data\pdfs\CS_NU18VKYW.pdf", mode="paged")
 docs = loader.load()
 
-# docs is a list of LangChain Documents. To inspect unstructured elements use unstructured.partition_pdf (next example)
 print(len(docs))
 print(docs[1].page_content)
 # print(docs[3])
